749c20771ca20a6e3f70fc5c3c7f80efc828f92e
Laif Harwood <laifharwood@gmail.com>
2015-11-13 13:51:14 -0700

    Initial Commit

---
+++ InvestorStalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InvestorStalk
+//
+//  Created by Laif Harwood on 11/13/15.
+//  Copyright © 2015 LaifHarwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
